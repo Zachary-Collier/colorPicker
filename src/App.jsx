87c85511cf48928ea79c0c0310d927a3fcd3337e
Zachary-Collier <zachary.collier@saintleo.edu>
2023-07-04 17:37:45 -0400

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import { useState } from "react";
+import { useState } from "react"; // Step 1 Import the useState hook from React //
 
 // Write your Color component here
 
-//const Color = (props) => {
-//  return <div className={props.color}></div>
-//}
-
 const Color = ({ color, setSelectedColor, selectedColor }) => {
-  const selectedClassName = selectedColor === color ? "selected" : "color";
-  return <div className={color} onClick={() => setSelectedColor(color)}></div>;
+  const selectedClassName = selectedColor === color ? 'selected' : 'color';
+  return (
+     <div className={selectedClassName} style={{background: color}} onClick={() => setSelectedColor(color)}></div>
+  );
 };
 
 const App = () => {
-  const [selectedColor, setSelectedColor] = useState("red");
+  const [selectedColor, setSelectedColor] = useState(
+    "Click on a colored circle"); // Step 2 On the first line of your Picker component, invoke the useState hook, and deconstruct the selectedColor and setSelectedColor from the returned array.
   return (
     <div id="container">
       <div id="navbar">
@@ -29,3 +28,11 @@ const App = () => {
 };
 
 export default App;
+
+// Step 3 is for line 17: Change the JSX of the Picker component so that it shows the currently selectedColor from state in the navbar.
+
+// Step 4 is for lines 20 thru 22: Pass the setSelectedColor function down to each of the Color components.
+
+// Step 5 is for line 7: In the Color component, add an onClick property with an anonymous function passed in.
+
+// Note: on line 5 simply added as an argument selectedColor and I created line 6 through a lot of my old notes.  I was able to get everything to work.  However, I wasn't able to figure out the last tab called Extension, and quite honestly the first tab called Initialize Project, I'm still having problems doing anything with github
